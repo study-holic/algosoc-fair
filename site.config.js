5b270f6508fed21ff4b7db4b0958299edc066fe1
study-holic <williamodumosuu@gmail.com>
2026-09-24 20:03:30 +0100

    Point stall QR at https://study-holic.github.io/algosoc-fair/

--- site.config.js
+++ site.config.js
@@ -3,6 +3,6 @@
    It is what the stall QR code (docs/qr.html, docs/qr.png, docs/qr.svg) points to.
    It can also be set for one build with the MOBILE_SITE_URL environment variable. */
 module.exports = {
-  MOBILE_SITE_URL: process.env.MOBILE_SITE_URL || 'https://USERNAME.github.io/REPOSITORY/',
+  MOBILE_SITE_URL: process.env.MOBILE_SITE_URL || 'https://study-holic.github.io/algosoc-fair/',
   JOIN_URL: 'https://linktr.ee/uom_algosoc',
 };
